6daab103f1c061b096e68bb38ab98116acaafc6c
tegareyy <tegarirfanhamid27@gmail.com>
2025-01-13 17:48:22 +0700

    setup laravel with tailwind CSS

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-gray-100 text-center p-10">
+    <h1 class="text-3xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+</body>
+</html>
