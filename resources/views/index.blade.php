0ab0b900d26a3d137f7f00ce3dac421cce412577
tegareyy <tegarirfanhamid27@gmail.com>
2025-01-14 16:27:58 +0700

    adding more file at resources'

---
+++ resources/views/index.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap">
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-cream2 text-center">
+
+    <!-- Navbar -->
+    <div class="col">
+        <div class="row">
+            <div class="flex justify-center mt-8">
+                <ul class="flex gap-x-24">
+                    <li>
+                        <a href="" class="font-semibold font-inter text-2xl">Home</a>
+                    </li>
+                    <li>
+                        <a href="" class="font-semibold font-inter text-2xl">Resource</a>
+                    </li>
+                    <li>
+                        <a href="" class="font-semibold font-inter text-2xl">Support</a>
+                    </li>
+                    <li>
+                        <a href="" class="font-semibold font-inter text-2xl">Pricing</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- end Navbar -->
+
+    <!-- Hero 1 -->
+    <div class="max-w-full max-h-full">
+        <div class="container">
+            <!-- foto atas -->
+            <div class="flex row justify-between">
+                <div class="col">
+                    <img src="../img/hero1.jpg" alt="" class="w-36 top-32 -right-56 relative">
+                </div>
+                <div class="col ">
+                    <img src="../img/hero1.jpg" alt="" class="w-32 h-44 top-44 -right-40 relative">
+                </div>
+                <div class="col ">
+                    <img src="../img/hero1.jpg" alt="" class="w-52 h-36 mt-36 left-28 relative">
+                </div>
+                <div class="col ">
+                    <img src="../img/hero1.jpg" alt="" class="w-36 h-56 -left-10 relative">
+                </div>
+            </div>
+            <!-- foto atas -->
+
+            <!-- text middle -->
+            <div class="row mt-[65px] font-serif text-6xl font-semibold">
+                <span class="font-light">SELAMAT DATANG DI GALERI</span>
+                <div class="font-light">
+                    BAMBANG TJAHJADI
+                </div>
+            </div>
+            <!-- text middle -->
+
+            <!-- foto bawah -->
+            <div class="flex row justify-between">
+                <div class="col">
+                    <img src="../img/hero1.jpg" alt="" class="w-28 h-32 -top-5 relative">
+                </div>
+                <div class="col ">
+                    <img src="../img/hero1.jpg" alt="" class="w-[140px] h-32 left-6 -bottom-14 relative">
+                </div>
+                <div class="col ">
+                    <img src="../img/hero1.jpg" alt="" class="w-60 h-32 mt-36 -left-12 -bottom-9 relative">
+                </div>
+                <div class="col ">
+                    <img src="../img/hero1.jpg" alt="" class="w-40 h-36 -top-2 -left-28 relative">
+                </div>
+            </div>
+            <!-- foto bawah -->
+        </div>
+
+
+    </div>
+    <!-- end Hero 1 -->
+
+
+    <!-- Hero 2 -->
+    <div class="w-full min-h-screen bg-blackDrop mt-10 pb-44 relative">
+        <div class="container mx-auto text-white justify-items-center">
+            <!-- text -->
+            <div class="row flex">
+                <div class="col text-5xl font-serif text-justify pt-24">
+                    <p class="uppercase">all type of</p>
+                    <p class="uppercase">project</p>
+                </div>
+                <div class="col text-xl ml-8 text-justify pt-44">
+                    <p>with superhub collab, organize and prioritize feature request to</p>
+                    <p>better understand with you any with superhub collab, organize</p>
+                    <p>and prioritize feature request to better understand with you</p>
+                    <p>any</p>
+                </div>
+            </div>
+            <!-- end text -->
+
+            <!-- image -->
+            <div class="flex row justify-between">
+                <div class="col">
+                    <img src="../img/hero1.jpg" alt="" class="w-56 -left-44 relative">
+                </div>
+                <div class="col ">
+                    <img src="../img/hero1.jpg" alt="" class="w-48 h-64 -left-24 -bottom-44 relative">
+                </div>
+                <div class="col ">
+                    <img src="../img/hero1.jpg" alt="" class="w-52 h-36 relative">
+                </div>
+                <div class="col ">
+                    <img src="../img/hero1.jpg" alt="" class="w-36 h-56  relative">
+                </div>
+            </div>
+            <!-- end image -->
+        </div>
+    </div>
+    <!-- end Hero 2 -->
+
+
+</body>
+
+</html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
     theme: {
         extend: {
+            colors: {
+                cream: "rgb(250, 243, 237)",
+                cream2: "#f9f2ed",
+                blackDrop: "#101010",
+            },
+
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                inter: ["Inter", "sans-serif", ...defaultTheme.fontFamily.sans],
             },
         },
     },
